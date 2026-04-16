5ec8e2cdf136eb6acb9f3b0977f849924e3ff46d
Kirill <kir@.mail.ru>
2026-04-16 20:19:18 +0700

    добавлен вход/выход по логину+регистрация

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -22,6 +22,10 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('add_user/',views.add_user),
     path('users/',views.users),
-    path('', views.main),
+    path('', views.index),
     path('add_role/', views.add_role),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
+
+
--- blog/login/views.py
+++ blog/login/views.py
@@ -35,3 +35,35 @@ def add_role(request):
         form = RoleForm()
     
     return render(request, "add_role.html", {'form': form})
+
+
+def index(request):
+    if request.session.get('user_id'):
+        id = request.session.get('user_id')
+        u = User.objects.get(id=id)
+        return render (request, 'index.html', {'user': u})
+    else:
+        return redirect('/login/')
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        login = request.POST.get('login')
+        password = request.POST.get('pas')
+
+        try:
+            user = User.objects.get(login=login)
+        except User.DoesNotExist:
+            return redirect('/login')
+
+        if password != user.password:
+            return redirect('/login')
+
+        request.session['user_id'] = user.id
+        request.session['login'] = user.login
+        return redirect('/', {'page': 'login'})
+
+def logout_view(request):
+    request.session.flush()
+    return redirect('/login', {'page': 'logout'})
